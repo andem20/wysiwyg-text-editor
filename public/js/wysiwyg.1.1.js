0d6453f92ec725af84ff4cf34838ea3dc37c0fe3
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2021-03-12 21:11:20 +0100

    Refactoring

--- public/js/wysiwyg.1.1.js
+++ public/js/wysiwyg.1.1.js
@@ -6,13 +6,13 @@ class WYSIWYG {
             "bold" : document.createElement("button"),
             "italic" : document.createElement("button"),
             "underline" : document.createElement("button"),
-			"link" : document.createElement("button")
+			"CreateLink" : document.createElement("button")
         };
+
         this.init();
     }
 
     createEditor() {
-
         // Create elements
         let menu = document.createElement("div");
 
@@ -22,66 +22,44 @@ class WYSIWYG {
 
         // Append children
 		this.container.append(menu, this.content);
-		menu.append(this.buttons["bold"], 
-					this.buttons["italic"], 
-					this.buttons["underline"], 
-					this.buttons["link"]
-				);
+		Object.values(this.buttons).forEach(e => menu.appendChild(e));
 
         // Add content
 		this.content.contentEditable = "true";
 		this.buttons["bold"].innerHTML = "<b>B</b>";
 		this.buttons["italic"].innerHTML = "<i>I</i>";
 		this.buttons["underline"].innerHTML = "<u>U</u>";
-		this.buttons["link"].innerHTML = "<img src='public/icons/link.png' height='10px' />";
+		this.buttons["CreateLink"].innerHTML = "<img src='public/icons/link.png' height='10px' />";
 	}
 
 
 	addStyle(style, value) {
-		value = (value) ? value : null;
     	document.execCommand(style, false, value);
 		this.buttons[style].classList.toggle("active");
-
-
     	this.content.focus();
 	}
 
 	checkType() {
-		if(document.queryCommandState("bold")){
-			this.buttons["bold"].classList.add("active");
-		} else {
-			this.buttons["bold"].classList.remove("active");
-		}
-
-		if(document.queryCommandState("italic")){
-			this.buttons["italic"].classList.add("active");
-		} else {
-			this.buttons["italic"].classList.remove("active");
-		}
-
-		if(document.queryCommandState("underline")){
-			this.buttons["underline"].classList.add("active");
-		} else {
-			this.buttons["underline"].classList.remove("active");
-		}
+		Object.keys(this.buttons).forEach(key => {
+			this.buttons[key].classList.remove("active");
+			if(document.queryCommandState(key)) this.buttons[key].classList.add("active");
+		});
 	}
 
 	init() {
 		this.createEditor();
         let self = this;
-		this.buttons["bold"].addEventListener("click", () => self.addStyle("bold"));
 
-		this.buttons["italic"].addEventListener("click", () => self.addStyle("italic"));
+		Object.keys(this.buttons).forEach(key => {
+			this.buttons[key].addEventListener("click", () => self.addStyle(key));
+		});
 
-		this.buttons["underline"].addEventListener("click", () => self.addStyle("underline"));
-
-		this.buttons["link"].addEventListener("click", () => {
-			var link = prompt("Please enter a link:", "Link");
+		this.buttons["CreateLink"].addEventListener("click", () => {
+			let link = prompt("Please enter a link:", "Link");
 			self.addStyle("CreateLink", link);
 		});
 
 		this.content.addEventListener("click", () => self.checkType(), false);
-
 		this.content.addEventListener("keydown", () => self.checkType(), false);
 
 		this.content.focus();
