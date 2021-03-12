90cd081e3c2e95b25006c27f6f82b83023f19ac6
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2021-03-12 20:53:27 +0100

    Refactor

--- public/js/wysiwyg.1.1.js
+++ public/js/wysiwyg.1.1.js
@@ -2,14 +2,11 @@ class WYSIWYG {
     constructor(container) {
         this.container = container;
         this.content = document.createElement("div");
-        this.boldBtn = document.createElement("button");
-        this.italicBtn = document.createElement("button");
-        this.underlineBtn = document.createElement("button");
-        this.linkBtn = document.createElement("button");
         this.buttons = {
-            "bold" : this.boldBtn,
-            "italic" : this.italicBtn,
-            "underline" : this.underlineBtn
+            "bold" : document.createElement("button"),
+            "italic" : document.createElement("button"),
+            "underline" : document.createElement("button"),
+			"link" : document.createElement("button")
         };
         this.init();
     }
@@ -24,64 +21,61 @@ class WYSIWYG {
 		menu.className = "wMenu";
 
         // Append children
-		this.container.appendChild(menu);
-		this.container.appendChild(this.content);
-		menu.appendChild(this.boldBtn);
-		menu.appendChild(this.italicBtn);
-		menu.appendChild(this.underlineBtn);
-		menu.appendChild(this.linkBtn);
+		this.container.append(menu, this.content);
+		menu.append(this.buttons["bold"], 
+					this.buttons["italic"], 
+					this.buttons["underline"], 
+					this.buttons["link"]
+				);
 
         // Add content
 		this.content.contentEditable = "true";
-		this.boldBtn.innerHTML = "<b>B</b>";
-		this.italicBtn.innerHTML = "<i>I</i>";
-		this.underlineBtn.innerHTML = "<u>U</u>";
-		this.linkBtn.innerHTML = "<img src='icons/link.png' height='10px' />";
+		this.buttons["bold"].innerHTML = "<b>B</b>";
+		this.buttons["italic"].innerHTML = "<i>I</i>";
+		this.buttons["underline"].innerHTML = "<u>U</u>";
+		this.buttons["link"].innerHTML = "<img src='public/icons/link.png' height='10px' />";
 	}
 
 
 	addStyle(style, value) {
 		value = (value) ? value : null;
     	document.execCommand(style, false, value);
-		if(document.queryCommandState(style)){
-			this.buttons[style].classList.add("active");
-		} else {
-			this.buttons[style].classList.remove("active");
-		}
+		this.buttons[style].classList.toggle("active");
+
 
     	this.content.focus();
 	}
 
 	checkType() {
 		if(document.queryCommandState("bold")){
-			this.boldBtn.classList.add("active");
+			this.buttons["bold"].classList.add("active");
 		} else {
-			this.boldBtn.classList.remove("active");
+			this.buttons["bold"].classList.remove("active");
 		}
 
 		if(document.queryCommandState("italic")){
-			this.italicBtn.classList.add("active");
+			this.buttons["italic"].classList.add("active");
 		} else {
-			this.italicBtn.classList.remove("active");
+			this.buttons["italic"].classList.remove("active");
 		}
 
 		if(document.queryCommandState("underline")){
-			this.underlineBtn.classList.add("active");
+			this.buttons["underline"].classList.add("active");
 		} else {
-			this.underlineBtn.classList.remove("active");
+			this.buttons["underline"].classList.remove("active");
 		}
 	}
 
 	init() {
 		this.createEditor();
         let self = this;
-		this.boldBtn.addEventListener("click", () => self.addStyle("bold"));
+		this.buttons["bold"].addEventListener("click", () => self.addStyle("bold"));
 
-		this.italicBtn.addEventListener("click", () => self.addStyle("italic"));
+		this.buttons["italic"].addEventListener("click", () => self.addStyle("italic"));
 
-		this.underlineBtn.addEventListener("click", () => self.addStyle("underline"));
+		this.buttons["underline"].addEventListener("click", () => self.addStyle("underline"));
 
-		this.linkBtn.addEventListener("click", () => {
+		this.buttons["link"].addEventListener("click", () => {
 			var link = prompt("Please enter a link:", "Link");
 			self.addStyle("CreateLink", link);
 		});
