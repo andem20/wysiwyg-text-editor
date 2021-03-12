b92e62f748296117f15c21d6033b16b9547dadb1
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2021-03-12 21:37:18 +0100

    Added text alignment feature

--- public/js/wysiwyg.1.1.js
+++ public/js/wysiwyg.1.1.js
@@ -6,7 +6,11 @@ class WYSIWYG {
             "bold" : document.createElement("button"),
             "italic" : document.createElement("button"),
             "underline" : document.createElement("button"),
-			"CreateLink" : document.createElement("button")
+			"createLink" : document.createElement("button"),
+			"justifyLeft" : document.createElement("button"),
+			"justifyCenter" : document.createElement("button"),
+			"justifyRight" : document.createElement("button"),
+			"justifyFull" : document.createElement("button")
         };
 
         this.init();
@@ -29,13 +33,18 @@ class WYSIWYG {
 		this.buttons["bold"].innerHTML = "<b>B</b>";
 		this.buttons["italic"].innerHTML = "<i>I</i>";
 		this.buttons["underline"].innerHTML = "<u>U</u>";
-		this.buttons["CreateLink"].innerHTML = "<img src='public/icons/link.png' height='10px' />";
+		this.buttons["createLink"].innerHTML = "<img src='public/icons/link.png' height='10px' />";
+		this.buttons["justifyLeft"].innerHTML = "<img src='public/icons/left-align.png' height='15px' />";
+		this.buttons["justifyCenter"].innerHTML = "<img src='public/icons/center-align.png' height='15px' />";
+		this.buttons["justifyRight"].innerHTML = "<img src='public/icons/right-align.png' height='15px' />";
+		this.buttons["justifyFull"].innerHTML = "<img src='public/icons/justify-align.png' height='15px' />";
 	}
 
 
 	addStyle(style, value) {
     	document.execCommand(style, false, value);
 		this.buttons[style].classList.toggle("active");
+		this.checkType();
     	this.content.focus();
 	}
 
@@ -51,12 +60,12 @@ class WYSIWYG {
         let self = this;
 
 		Object.keys(this.buttons).forEach(key => {
-			this.buttons[key].addEventListener("click", () => self.addStyle(key));
+			self.buttons[key].addEventListener("click", () => self.addStyle(key));
 		});
 
-		this.buttons["CreateLink"].addEventListener("click", () => {
+		this.buttons["createLink"].addEventListener("click", () => {
 			let link = prompt("Please enter a link:", "Link");
-			self.addStyle("CreateLink", link);
+			self.addStyle("createLink", link);
 		});
 
 		this.content.addEventListener("click", () => self.checkType(), false);
